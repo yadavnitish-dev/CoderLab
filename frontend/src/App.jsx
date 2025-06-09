052d2eb42ffe367483f6fb9a9ce3eb15148f49b9
Nitish Yadav <yadavnitish00028@gmail.com>
2025-06-09 23:58:46 +0530

    added login, signup and homepage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-function App(){
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+const App = () => {
   return (
-  <div>
-    <h1>Hello World!!</h1>
-  </div>
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route 
+        path="/"
+        element= {<HomePage />}
+        />
+
+        <Route 
+        path="/login"
+        element={<Login />}
+        />
+
+        <Route 
+        path="/signup"
+        element = {<SignUp />}
+        />
+
+
+      </Routes>
+    </div>
+    
   )
 }
 
