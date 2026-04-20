c23ec40d3c1ffb68e90090f79d1b2c9d43ff0122
Nitish Yadav <yadavnitish00028@gmail.com>
2026-04-21 02:42:53 +0530

    refactor: modularize JWT logic, add OAuth support, update rate limits, and exclude dist from git tracking.

--- backend/dist/routes/executeCode.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middleware/auth.middleware.js";
-import { executeCode } from "../controllers/executeCode.controller.js";
-const executionRoutes = express.Router();
-executionRoutes.post("/", authMiddleware, executeCode);
-export default executionRoutes;
--- backend/dist/src/services/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./errors.js";
-export { authService, AuthService } from "./auth.service.js";
-export { problemService, ProblemService } from "./problem.service.js";
-export { submissionService, SubmissionService } from "./submission.service.js";
-export { playlistService, PlaylistService } from "./playlist.service.js";
-export { codeExecutionService, CodeExecutionService } from "./executeCode.service.js";
